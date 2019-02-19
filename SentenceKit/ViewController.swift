ccb7073a8ac5f634aeafaeee31a6170901708110
Ricky Kirkendall <ricky.kirkendall@heal.com>
2019-02-18 21:50:50 -0800

    Initial Commit

---
+++ SentenceKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SentenceKit
+//
+//  Created by Ricky Kirkendall on 2/18/19.
+//  Copyright © 2019 Ricky Kirkendall. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
