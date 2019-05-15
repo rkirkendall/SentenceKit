4dbe40b44ca58764e80c6e53b83748e838db1ff4
Ricky Kirkendall <rkirkendall304@gmail.com>
2019-05-14 21:53:42 -0700

    Improved logic for multichoice

--- SentenceKit/Sentence UI/Model/MultiChoice/MultiChoice.swift
+++ SentenceKit/Sentence UI/Model/MultiChoice/MultiChoice.swift
@@ -14,18 +14,10 @@ public class MultiChoice: ControlFragment {
     var options: [String] {
         didSet {
             _editViewController.choices = self.options
+            string = options.count > 0 ? options[0] : placeholder
         }
     }
     private var _editViewController = MultiChoiceEditViewController()
-    
-    override var alias: String? {
-        get {
-            return super.alias ?? (options.count > 0 ? options[0] : placeholder)
-        }
-        set {
-            super.alias = newValue
-        }
-    }
         
     override init() {
         options = []
